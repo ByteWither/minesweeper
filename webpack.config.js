02ef01f49b2f5592681861b1f74987999d112156
Ветчинкин Александр <avetchinkin@awg.ru>
2021-09-06 14:12:05 +0300

    build(webpack): typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,19 @@ const path = require('path');
 module.exports = () => {
     return {
         entry: {
-            main: path.resolve(__dirname, './src/index.js'),
+            main: path.resolve(__dirname, './src/index.ts'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x?)$/,
+                    use: 'ts-loader',
+                    exclude: /[\\/]node_modules[\\/]/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
         },
         output: {
             path: path.resolve(__dirname, './dist'),
