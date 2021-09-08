850174e952dc352a19875b0177634c52e906b54d
Ветчинкин Александр <avetchinkin@awg.ru>
2021-09-08 15:46:51 +0300

    feat(board): generate board with fields

--- src/App/Board.tsx
+++ src/App/Board.tsx
@@ -1,9 +1,20 @@
 function Field() {
-    return <div className="field"></div>;
+    return (<button></button>)
+    
 }
 
 function Board() {
-    return <div className="board">Hi</div>;
+    return (
+        <div className="board">
+            for (let i: number = 0; i < 10; i++) {
+                <div className="board-row">
+                    for (let j: number = 0; j < 10; j++) {
+                        <Field />
+                    }
+                </div>
+            }
+        </div>
+    )
 }
 
 export default Board;
