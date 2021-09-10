2bef02bda2030b2c63405b48cf9a8c184976da35
Alexander_V <moki1444@gmail.com>
2021-09-10 15:58:21 +0300

    refactor(board): replace functions in export

--- src/App/Board.tsx
+++ src/App/Board.tsx
@@ -1,29 +1,31 @@
 import React, { useState } from "react"
 import { Field } from "./Field"
 
-function boardRow(i: number) {
-    // const [cords, setCords] = useState({ x: 0, y: 0 })
-    const fields = []
-    for (let j: number = 0; j < 10; j++) {
-        fields.push(<Field key={j.toString()} x={j} y={i}></Field>)
-    }
-    return fields
-}
-
-function fields() {
-    const rows = []
-    for (let i: number = 0; i < 10; i++) {
-        rows.push(<div className="board-row">{boardRow(i)}</div>)
-    }
-    return rows
-}
-
-function generateBoard() {
-    console.log("Board generated!")
-}
+// function setBomb() {}
 
 export function Board() {
     const [generateBoards, setGenerateBoards] = useState(0)
+
+    function boardRow(i: number) {
+        // const [cords, setCords] = useState({ x: 0, y: 0 })
+        const fields = []
+        for (let j: number = 0; j < 10; j++) {
+            fields.push(<Field key={j.toString()} x={j} y={i}></Field>)
+        }
+        return fields
+    }
+
+    function fields() {
+        const rows = []
+        for (let i: number = 0; i < 10; i++) {
+            rows.push(<div className="board-row">{boardRow(i)}</div>)
+        }
+        return rows
+    }
+
+    function generateBoard() {
+        console.log("Board generated!")
+    }
 
     if (generateBoards === 1) {
         generateBoard()
