760fa545f6730d58d7da60abd1b507b87a797388
Alexander_V <moki1444@gmail.com>
2021-09-21 13:25:27 +0300

    feat(board): adding victory and defeat

--- src/App/Board.tsx
+++ src/App/Board.tsx
@@ -76,17 +76,41 @@ const getPointsAround = (x: number, y: number) => {
     return result
 }
 
+const getOpenedCells = (board: board, countOpenCells: number) => {
+    board.forEach((row) => {
+        row.map((cell) => {
+            if (cell.opened) {
+                countOpenCells--
+            }
+        })
+    })
+    return countOpenCells
+}
+
+const getMinesCells = (board: board) => {
+    board.forEach((row) => {
+        row.map((cell) => {
+            if (cell.isMine) {
+                cell.opened = true
+            }
+        })
+    })
+}
+
 type boardProps = {
     difficulty: string
 }
 
 export function Board({ difficulty }: boardProps) {
     const firstStep = React.useRef<boolean>(true)
+    // const [difficultyLevel, setDifficultyLevel] = React.useState(difficulty)
     const [userBoard, setUserBoard] = React.useState<board>([])
 
     let ROWS: number
     let CELLS: number
     let MINESCOUNT: number
+
+    console.log(difficulty)
 
     switch (difficulty) {
         case "Easy":
@@ -105,6 +129,7 @@ export function Board({ difficulty }: boardProps) {
             MINESCOUNT = 99
             break
     }
+    const countOpenCells = React.useRef(ROWS * CELLS - MINESCOUNT)
 
     const boardFill = () => {
         const board: board = []
@@ -195,11 +220,23 @@ export function Board({ difficulty }: boardProps) {
 
             if (mutateBoard[x][y].isMine && !mutateBoard[x][y].flagged) {
                 console.log("Game over")
+                getMinesCells(mutateBoard)
                 return mutateBoard
             }
 
             if (!mutateBoard[x][y].flagged) {
                 mutateBoard[x][y].opened = true
+            }
+
+            return mutateBoard
+        })
+
+        setUserBoard((prevState) => {
+            const mutateBoard = cloneDeep(prevState)
+            let count = getOpenedCells(mutateBoard, countOpenCells.current)
+
+            if (count === 0) {
+                console.log("Win!")
             }
 
             return mutateBoard
@@ -224,8 +261,6 @@ export function Board({ difficulty }: boardProps) {
             return "flag"
         } else if (field.opened) {
             return "opened"
-        } else if (field.isMine) {
-            return "bomb"
         }
         return null
     }
@@ -237,6 +272,15 @@ export function Board({ difficulty }: boardProps) {
             return field.minesAround
         }
         return null
+    }
+
+    const openBombs = (x: number, y: number) => {
+        const field = userBoard[x][y]
+
+        if (field.opened && field.isMine) {
+            return true
+        }
+        return false
     }
 
     const getClass = (x: number, y: number) => {
@@ -258,10 +302,11 @@ export function Board({ difficulty }: boardProps) {
                             key={"" + rowIndex + cellIndex}
                             x={rowIndex}
                             y={cellIndex}
-                            minesAround={getActualNumber(rowIndex, cellIndex)}
                             state={getActualState(rowIndex, cellIndex)}
                             leftClick={leftHandle}
                             rightClick={rightHandle}
+                            minesAround={getActualNumber(rowIndex, cellIndex)}
+                            openBombs={openBombs(rowIndex, cellIndex)}
                             className={getClass(rowIndex, cellIndex)}
                         />
                     ))}
