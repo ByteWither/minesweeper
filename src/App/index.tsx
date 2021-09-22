94b74ad202bc6e26dd221464867808e4f557319a
Alexander_V <moki1444@gmail.com>
2021-09-22 13:54:44 +0300

    refactor(app): replace app into index.tsx

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,39 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { App } from "./App"
-import "../Sass/style.sass"
+import { Board } from "Components/Board"
+import { Face, FaceTypes } from "Components/Face"
+import { Select } from "Components/Select"
+
+const DIFFICULTIES: Select = [
+    { value: "easy", title: "Easy" },
+    { value: "normal", title: "Normal" },
+    { value: "hard", title: "Hard" },
+]
+
+function App() {
+    const [diff, setDiff] = React.useState<string>("easy")
+    const [gameState, setGameState] = React.useState<FaceTypes>(null)
+
+    const changeDifficulty = React.useCallback((value: string) => {
+        setDiff(value)
+    }, [])
+
+    const changeEmoji = React.useCallback((value: FaceTypes) => {
+        setGameState(value)
+    }, [])
+
+    require("./index.sass")
+
+    return (
+        <>
+            <div className="menu">
+                <Select options={DIFFICULTIES} value={diff} onChange={changeDifficulty} />
+                <Face state={gameState} />
+            </div>
+            <Board difficulty={diff} onGameState={changeEmoji} />
+        </>
+    )
+}
 
 ReactDOM.render(
     <React.StrictMode>
